4a04960fd2585361ca5c837c8fe4e0d515d2ffca
umayr43 <umayr43@gmail.com.com>
2026-02-11 18:19:29 +0530

    fixed issue in aws_s3_bucket notification

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,7 +119,7 @@ resource "aws_s3_bucket_notification" "input_notification" {
 
     lambda_function {
       lambda_function_arn = aws_lambda_function.data_processor.arn
-      events=["s3ObjectCreated:*"]
+      events=["s3:ObjectCreated:*"]
       filter_suffix = ".csv"
     }
     depends_on = [aws_lambda_permission.allow_s3]
