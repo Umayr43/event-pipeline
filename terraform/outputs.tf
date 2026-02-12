5270173c0b7e45805cd4e78eb774ee4e1c696dcc
umayr43 <umayr43@gmail.com.com>
2026-02-13 00:37:28 +0530

    created test sample data and fixed outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "input_bucket" {
-    value = aws_s3_bucket.input_bucket.id
+    value = aws_s3_bucket.input_bucket
 }
 output "processed_bucket" {
-    value = aws_s3_bucket.processed_bucket.id
+    value = aws_s3_bucket.processed_bucket
 }
 output "report_bucket" {
-    value = aws_s3_bucket.report_bucket.id
+    value = aws_s3_bucket.report_bucket
 }
