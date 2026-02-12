f5e399b4dc077aa6a8d7cf04aadc26c8694b811f
umayr43 <umayr43@gmail.com.com>
2026-02-13 02:14:49 +0530

    fixed outputs.tf to just return bucket name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "input_bucket" {
-    value = aws_s3_bucket.input_bucket
+    value = aws_s3_bucket.input_bucket.bucket
 }
 output "processed_bucket" {
-    value = aws_s3_bucket.processed_bucket
+    value = aws_s3_bucket.processed_bucket.bucket
 }
 output "report_bucket" {
-    value = aws_s3_bucket.report_bucket
+    value = aws_s3_bucket.report_bucket.bucket
 }
